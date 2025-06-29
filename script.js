be63f6d1145c4ff827b720ad23876facc12838e4
ourarimondher <ourari.mondher.ben.ali@gmail.com>
2025-06-29 18:58:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-js
+
+document.addEventListener("DOMContentLoaded", function () {
+  var colorBox = document.getElementById("color-box");
+  var changeColorBtn = document.getElementById("change-color-btn");
+
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  changeColorBtn.addEventListener("click", function () {
+    var newColor = getRandomColor();
+    colorBox.style.backgroundColor = newColor;
+  });
+});
+
